8d3675041594f9686f4cfb9bd42d5054315909ab
ZarkoTimacum <zarko@timacum.com>
2023-02-03 12:00:46 +0100

    new file:   src/abi.json 	new file:   src/functions.ts 	new file:   src/main.ts 	new file:   src/style.css

---
+++ src/functions.ts
@@ -0,0 +1,155 @@
+import {
+	createClient,
+	configureChains,
+	watchAccount,
+	disconnect,
+} from '@wagmi/core';
+import { connect } from '@wagmi/core';
+import { InjectedConnector } from '@wagmi/core/connectors/injected';
+import { alchemyProvider } from '@wagmi/core/providers/alchemy';
+import { goerli } from '@wagmi/core/chains';
+import { prepareWriteContract } from '@wagmi/core';
+import { writeContract } from '@wagmi/core';
+import { ethers } from "ethers";
+import { readContract } from '@wagmi/core';
+import { waitForTransaction } from '@wagmi/core';
+import ABI  from './abi.json';
+
+// main wagmi configuration setup.
+export function configureMainWagmi() {
+	const { provider, webSocketProvider } = configureChains(
+		[goerli],
+		[alchemyProvider({ apiKey: 'RGl6ALYLxiuktwSl7e0LbzuNwWM8rZPB' })], 
+	);
+	const client = createClient({
+		autoConnect: true,
+		provider,
+		webSocketProvider,
+	});
+}
+
+window.onload= async function NumberOfCars () {
+	const data = await readContract({
+		address: '0x7B47Cc733F49A7aF3fF4885579FbAa302E8d84ca',
+		abi: ABI,
+		functionName: 'brojAuta',
+	  })
+	  console.log("Number of car test  " + data); 
+	
+	  document.getElementById("write").innerHTML="Total number of cars: " + data;
+	}
+
+	export async function CreateCar () {
+	
+	try {
+	document.getElementById("creating").innerHTML="Creating...";
+	document.getElementById("enabled").style.backgroundColor= '#B7B7B7';
+	document.getElementById("enabled").disabled = true;
+	
+	let Registered;
+
+	if (document.getElementById("myCheck").checked){
+			Registered = true;
+	}
+	else {
+			Registered = false;
+	}
+	console.log(Registered);
+
+	let Name=document.getElementById("NameOfCar").value;
+	console.log(Name);
+					
+	let Color=document.getElementById("ColorOfCar").value;
+	console.log(Color);
+
+	let Year=document.getElementById("YearOfCar").value;
+	console.log(Year);
+
+	const config  = await prepareWriteContract({
+		address: '0x7B47Cc733F49A7aF3fF4885579FbAa302E8d84ca',
+		abi: ABI,
+		functionName: 'createCar',
+		args:[Color, Name, Year, Registered],
+		overrides: {
+		value: ethers.utils.parseEther('0.01'),
+				},
+	})
+		const { hash } = await writeContract(config);
+		console.log(hash);
+		
+		const HashTransaction = await waitForTransaction({
+			hash
+		  })
+	
+		  if(HashTransaction) {
+		
+		document.getElementById("enabled").style.backgroundColor= '#000000';
+		document.getElementById("creating").innerHTML="Successfully created new car";
+		document.getElementById("creating").style.color = '#1C872D';
+		document.getElementById("enabled").style.backgroundColor= '#000000';
+		document.getElementById("enabled").disabled = false;
+
+		const NewCar = await readContract({
+			address: '0x7B47Cc733F49A7aF3fF4885579FbAa302E8d84ca',
+			abi: ABI,
+			functionName: 'brojAuta',
+		})
+		document.getElementById("write").innerHTML="Total number of cars is: " + NewCar;
+	}
+				} catch (error) {
+			document.getElementById("creating").innerHTML="Error creating new car!";
+			document.getElementById("creating").style.color = '#FF0000';
+			document.getElementById("enabled").style.color = '#FFFFFF';
+			document.getElementById("enabled").disabled = false;
+			document.getElementById("enabled").style.backgroundColor= '#000000';
+				}
+			}
+		
+export function setupConnect(element: HTMLButtonElement) {
+	element.addEventListener('click', () => connectWallet());
+}
+export function setupDisconnect(element: HTMLButtonElement) {
+	element.addEventListener('click', () => disconnect());
+}
+export function Create(element: HTMLButtonElement) {
+	element.addEventListener('click', () => CreateCar());
+}
+export async function connectWallet() {
+	try {
+		const result = await connect({
+			connector: new InjectedConnector(),
+		});
+		console.log(result);
+		return result;
+	} catch (error) {
+		console.log(error);
+	}
+}
+export function watchAccountSetup(
+	connectElement: HTMLButtonElement,
+	disconnectElement: HTMLButtonElement,
+	addressElement: HTMLDivElement,
+	enabledcreate: HTMLButtonElement,
+	disabledcreate: HTMLButtonElement,
+	createElement: HTMLInputElement
+) {
+	const unwatch = watchAccount(
+		({ address, isConnected }) => {
+		
+			if (isConnected) {
+				connectElement.style.display = 'none';
+				disconnectElement.style.display = 'block';
+
+				enabledcreate.style.display = 'block';
+				disabledcreate.style.display = 'none';
+			} else {
+				connectElement.style.display = 'block';
+				disconnectElement.style.display = 'none';
+
+				enabledcreate.style.display = 'none';
+				disabledcreate.style.display = 'block';
+			}
+			addressElement.innerHTML = `${address ? '<h4>Your public address is: <br>' + address : '<h4>Connect your wallet first.<h4>'}`;
+		}
+	);
+}
---
+++ src/main.ts
@@ -0,0 +1,57 @@
+// styles
+import './style.css';
+
+// functions
+import {
+	configureMainWagmi,
+	setupConnect,
+	watchAccountSetup,
+	setupDisconnect,
+	Create
+} from './functions';
+
+// HTML part
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /* html */ `
+<div id="main">
+		<button id='connectBtn'>LOGIN</button>
+		<button id='disconnectBtn'>LOGOUT</button>
+		<div id='address'></div>
+<div id="container">
+<form id="form" onsubmit="return false">
+	 <div id="field">
+		   	<label for="Name">NAME:</label>
+		   	<input id="NameOfCar" type="text" maxlength="20" onchange="(this.value)"/><br>
+		  
+		   	<label for="Color">COLOR:</label>
+		   	<input id="ColorOfCar" type="text" maxlength="10" onchange="(this.value)"/><br>
+		  
+		   	<label for="Number">YEAR:</label>
+		   	<input id="YearOfCar" type="number" min="1950" max="2024" onchange="(this.value)"/><br>
+	  </div>   
+			<div id="checkdiv">
+						REGISTERED: <br> <input type="checkbox"  id="myCheck"/>
+						<button id='enabled'>CREATE CAR</button> 
+						<button id='disabled'>CREATE CAR</button> 
+			</div>
+				<div id="creating">
+				</div>
+		</form>
+	</div>
+	<p id="write">Total number of cars is: loading.... </p>
+</div> 
+  </div>
+`;
+
+// JavaScript part
+configureMainWagmi();
+watchAccountSetup(
+	document.querySelector<HTMLButtonElement>('#connectBtn')!,
+	document.querySelector<HTMLButtonElement>('#disconnectBtn')!,
+	document.querySelector<HTMLDivElement>('#address')!,
+	document.querySelector<HTMLButtonElement>('#enabled')!,
+	document.querySelector<HTMLButtonElement>('#disabled')!,
+	document.querySelector<HTMLInputElement>('#myChecked')!,
+);
+setupConnect(document.querySelector<HTMLButtonElement>('#connectBtn')!);
+setupDisconnect(document.querySelector<HTMLButtonElement>('#disconnectBtn')!);
+Create(document.querySelector<HTMLButtonElement>('#enabled')!);
